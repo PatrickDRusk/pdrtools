958d42fce4d788b2c97611155880001d7755f237
Patrick Rusk <PatrickDRusk@gmail.com>
2020-02-26 15:50:16 -0500

    Added --raw

--- pdrtools/msgpacker.py
+++ pdrtools/msgpacker.py
@@ -13,6 +13,7 @@ Options:
     --bucket BUCKET             The S3 bucket [default: cm-engineers]
     --prefix PREFIX             A common prefix for the targets [default: pdr/blobz]
     --json                      Add if input should be processed by json.loads() first
+    --raw                       Add if input should be completely unprocessed, even by msgpack
 
     -h --help                   Display this message
 
@@ -41,6 +42,11 @@ def __main():
     prefix = args['--prefix']
     target = args['<target>']
     is_json = args['--json']
+    is_raw = args['--raw']
+
+    if is_json and is_raw:
+        print("--json and --raw are not allowed together", file=sys.stderr)
+        sys.exit(1)
 
     s3_bucket = boto3.resource('s3').Bucket(bucket)
 
@@ -48,12 +54,12 @@ def __main():
     if is_json:
         data = json.loads(data)
 
-    write_blob(s3_bucket, prefix, target, data)
+    write_blob(s3_bucket, prefix, target, data, is_raw)
 
 
-def write_blob(s3_bucket, prefix, target, data):
+def write_blob(s3_bucket, prefix, target, data, is_raw):
     path = os.path.join(*filter(None, [prefix, target]))
-    bytes_ = msgpack.packb(data, use_bin_type=True)
+    bytes_ = data if is_raw else msgpack.packb(data, use_bin_type=True)
     s3_bucket.Object(path).put(Body=bytes_)
 
 
